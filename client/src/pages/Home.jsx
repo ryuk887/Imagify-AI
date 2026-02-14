2d74b9c37f3b5c55851180d47e5f1d0586849480
Ayush <mayushsharma627@gmail.com>
2026-02-15 04:28:14 +0530

    Home Page frontend completed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
+import React from 'react';
+import Header from '../components/Header';
+import Steps from '../components/Steps';
+import Description from '../components/Description';
+import Testimonials from '../components/Testimonials';
+import GenerateBtn from '../components/GenerateBtn';
 const Home = () => {
   return (
-    <div></div>
-  )
-}
+    <div>
+      <Header />
+      <Steps />
+      <Description />
+      <Testimonials />
+      <GenerateBtn />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
